2e387613b9a26d1a4a4e885338942ee00746f9b5
BonggXz <akunbonggt@gmail.com>
2025-06-21 21:52:05 +0700

    Add logout helper and profile page

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -1,9 +1,14 @@
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import useUser from "./useUser";
+
 export default function useAuth(redirect = "/auth/login") {
-  const user = useUser();
+  const { user } = useUser();
   const router = useRouter();
-  useEffect(() => { if (!user) router.push(redirect); }, [user, router]);
+
+  useEffect(() => {
+    if (!user) router.push(redirect);
+  }, [user, router]);
+
   return user;
 }
